971a01937ab22c4d7c95443da11c01ef7a049616
piialairi <piia.lairi@myy.haaga-helia.fi>
2023-09-05 13:08:33 +0300

    Spring Boot added

---
+++ src/main/java/com/op2/op2/Op2Application.java
@@ -0,0 +1,13 @@
+package com.op2.op2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Op2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Op2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/op2/op2/Op2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.op2.op2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Op2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
