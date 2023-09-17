32eace6afa50a88876b9be90671c6f62f698e49c
dovile-mart <dovile.martinonyte@gmail.com>
2023-09-17 17:23:49 +0300

    contextLoads-test case edited

--- src/test/java/com/op2/op2/Op2ApplicationTests.java
+++ src/test/java/com/op2/op2/Op2ApplicationTests.java
@@ -1,13 +1,27 @@
 package com.op2.op2;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.op2.op2.web.EventController;
+import com.op2.op2.web.LocationController;
 
 @SpringBootTest
 class Op2ApplicationTests {
-
+/* 
 	@Test
 	void contextLoads() {
 	}
-
+*/
+	@Autowired
+	private EventController eventController;
+	@Autowired
+	private LocationController locationController;
+	@Test
+	public void contextLoads() throws Exception {
+		assertThat(eventController).isNotNull();
+		assertThat(locationController).isNotNull();
+	}
 }
