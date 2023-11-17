9edea422adf59a3c59abfd9666911e2717ec1202
romeorusi <Romeo.rusi@gmail.com>
2023-11-17 13:19:51 +0200

    clicking logo takes you home

--- frontend/src/navigation/TabMUI.jsx
+++ frontend/src/navigation/TabMUI.jsx
@@ -29,8 +29,12 @@ function TabMUI() {
     <>
       <AppBar position='sticky' sx={{mt:0, mb:'36px'}}>
         <Toolbar>
-          <Box sx={{ flexGrow: 1, justifyContent: 'space-between'}}>
-            {logo && <img src={logo} alt="MTT Logo" style={{ height: '150px', width: 'auto', marginTop:"5px"}} />}
+          <Box sx={{ flexGrow: 1, justifyContent: 'space-between', verticalAlign: "left"}}>
+          {logo && (
+              <Link to='/'>
+                <img src={logo} alt="MTT Logo" style={{ height: '150px', width: 'auto', marginTop: "5px" }} />
+              </Link>
+            )}
           </Box>
           <Box sx={{ flexGrow: 1 }}>
           <Tabs value={value} onChange={handleChange} variant='fullWidth' centered selectionFollowsFocus textColor='inherit' TabIndicatorProps={{ style: { background: 'white' } }}>
